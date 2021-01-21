aa9e72e6162b073a5e079d6784d7764f10682be5
NERG-ITTG <normae8011@gmail.com>
2021-01-20 21:00:51 -0600

    se agregaron las migraciones

---
+++ database/migrations/2021_01_20_193504_create_tickets_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedSmallInteger('type_id')->nullable();
+            $table->unsignedSmallInteger('priority_id')->required();
+            $table->unsignedBigInteger('requester_user_id')->required();
+            $table->string('name')->required();
+
+            $table->boolean('flag_status')->default(true);
+
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by')->index();
+            $table->unsignedBigInteger('updated_by')->index();
+
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
+            $table->foreign('priority_id')->references('id')->on('priorities');
+            $table->foreign('requester_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
